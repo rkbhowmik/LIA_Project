d74e1d8905ffcb253f3237b76dc76af6f0f23494
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-04-18 10:31:49 +0200

    - Configure CourseContext class

--- LIA.Data/Data/CourseContext.cs
+++ LIA.Data/Data/CourseContext.cs
@@ -22,6 +22,29 @@ namespace LIA.Data.Data
         //public DbSet<User> Users { get; set; }
         public DbSet<UserCourse> UserCourses{ get; set; }
 
+        public CourseContext(DbContextOptions<CourseContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Composite key
+            builder.Entity<UserCourse>().HasKey(uc => new { uc.UserId, uc.CourseId });
+
+            // Restrict cascading deletes
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+        }
 
     }
+
+    
+
 }
