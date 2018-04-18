2ff083390139b491a6f9c8761ee3f836b98df9e7
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-04-18 10:02:42 +0200

    -CourseContext file created

---
+++ LIA.Data/Data/CourseContext.cs
@@ -0,0 +1,27 @@
+﻿using LIA.Data.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LIA.Data.Data
+{
+    public class CourseContext : IdentityDbContext<User>
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<ItemType> ItemTypes { get; set; }
+
+        public DbSet<Module> Modules { get; set; }
+
+        //public DbSet<User> Users { get; set; }
+        public DbSet<UserCourse> UserCourses{ get; set; }
+
+
+    }
+}
