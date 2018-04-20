d23b898fc297877d14aeddb0d5474118765f3cf7
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-04-20 12:30:17 +0200

    -Created all folders corresponding entities

--- LIA.Admin/Pages/Courses/Index.cshtml.cs
+++ LIA.Admin/Pages/Courses/Index.cshtml.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using LIA.Data.Data;
 using LIA.Data.Data.Entities;
 
 namespace LIA.Admin.Pages.Courses
