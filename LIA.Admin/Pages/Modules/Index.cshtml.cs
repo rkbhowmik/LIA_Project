f9c131778333c403fde1f0542666ab35d025efc3
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-04-20 12:43:00 +0200

    - code cleaned from home page

---
+++ LIA.Admin/Pages/Modules/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LIA.Data.Data;
+using LIA.Data.Data.Entities;
+
+namespace LIA.Admin.Pages.Modules
+{
+    public class IndexModel : PageModel
+    {
+        private readonly LIA.Data.Data.CourseContext _context;
+
+        public IndexModel(LIA.Data.Data.CourseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Module> Module { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Module = await _context.Modules.ToListAsync();
+        }
+    }
+}
