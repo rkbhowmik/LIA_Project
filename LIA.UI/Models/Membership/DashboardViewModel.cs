2f833359eb2bffb4fc323492316579d00054847d
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-04-27 10:02:24 +0200

    -DashboardViewModel created

---
+++ LIA.UI/Models/Membership/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LIA.Data.Data.Entities;
+using System.Collections.Generic;
+
+namespace LIA.UI.Models.Membership
+{
+    public class DashboardViewModel
+    {
+        public List<List<Course>> Courses { get; set; }
+    }
+}
