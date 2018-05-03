863bc5130d2cf80ffd19d20fe302008f7d33c664
Rajib Bhowmik <rajib.bhowmik@gmail.com>
2018-05-03 09:29:05 +0200

    UserCourses pages created

---
+++ LIA.Admin/Pages/UserCourses/Create.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using LIA.Data.Data.Entities;
+using LIA.Data.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LIA.Admin.Pages.UserCourses
+{
+    
+    public class CreateModel : PageModel
+    {
+        
+    }
+}
---
+++ LIA.Admin/Pages/UserCourses/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LIA.Data.Data.Entities;
+using LIA.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LIA.Admin.Pages.UserCourses
+{
+    public class IndexModel : PageModel
+    {
+        IDbReader _db;
+
+        public IList<UserCourse> UserCourses { get; set; }
+
+        public IndexModel(IDbReader db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+            UserCourses = _db.GetWithIncludes<UserCourse>().ToList();
+        }
+    }
+}
